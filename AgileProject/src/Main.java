69184c86eeab5e82219351d78e0d269eb3a9e799
alexander.graham@mycit.ie <33347630+Graham115@users.noreply.github.com>
2018-11-21 12:43:08 +0000

    Put in print statement for a change

--- AgileProject/src/Main.java
+++ AgileProject/src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		double num = mathFunctions.squareNumber(10);
 		System.out.println(num);
 		System.out.println(mathFunctions.addIntegers(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		System.out.println("Agile Processes Project");
 	}
 }
