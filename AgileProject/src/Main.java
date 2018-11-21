4238430a92e6dbaad8576c1913fc145183116ec1
alexander.graham@mycit.ie <33347630+Graham115@users.noreply.github.com>
2018-11-21 12:07:25 +0000

    Removal of maven and add java project

---
+++ AgileProject/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		MathFunctions mathFunctions = new MathFunctions();
+		double num = mathFunctions.squareNumber(10);
+		System.out.println(num);
+		System.out.println(mathFunctions.addIntegers(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	}
+}
---
+++ AgileProject/src/MathFunctions.java
@@ -0,0 +1,13 @@
+public class MathFunctions {
+	public double squareNumber(double num) {
+		return num * num;
+	}
+
+	public int addIntegers(int... a) {
+		int sum = 0;
+		for (int num : a) {
+			sum += num;
+		}
+		return sum;
+	}
+}
