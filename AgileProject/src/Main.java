b5267e8a542a7f0040dae46557ba4941535f69ae
alexander.graham@mycit.ie <33347630+Graham115@users.noreply.github.com>
2018-11-21 12:51:59 +0000

    Added another print statement

--- AgileProject/src/Main.java
+++ AgileProject/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		double num = mathFunctions.squareNumber(10);
 		System.out.println(num);
 		System.out.println(mathFunctions.addIntegers(1, 2, 3, 4, 5, 6, 7, 8, 9));
-		System.out.println("Agile Processes Project");
+		System.out.println("Agile Processes Project ");
+		System.out.println("2018");
 	}
 }
